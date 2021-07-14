a2a075c28e51bd6233b5d9eb0c043616c19ad702
Anthony Bonilla <anthonybonilla@ogremix.fios-router.home>
2021-07-14 09:53:17 -0400

    second update

---
+++ script.js
@@ -0,0 +1,20 @@
+const bill = document.querySelector('.bills');
+
+bill.addEventListener('click', () => {
+    const numPpl = document.getElementById('numPeople');
+    const tipAll = document.querySelectorAll('.tipsy')
+    const tip_5 = document.getElementById('tip-5');
+    const tip_10 = document.getElementById('tip-5');
+    const tip_15 = document.getElementById('tip-5');
+    const tip_25 = document.getElementById('tip-5');
+    const tip_50 = document.getElementById('tip-5');
+
+    console.log(25);
+
+    tipAll.forEach ( (e) => {
+        console.log(e)
+    })
+})
+
+console.log(25);
+console.log("25");
